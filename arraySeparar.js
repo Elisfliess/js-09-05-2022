034be1196cf6d88c3f3a0fcfb2a5d2bd35b1e6a1
ElisFliess <102757856+Elisfliess@users.noreply.github.com>
2022-05-09 15:30:29 -0300

    Add files via upload

---
+++ arraySeparar.js
@@ -0,0 +1,8 @@
+const chamada = ['Rai', 'ruan', 'kevin', 'MaridasDores', 'Felipe', 'Danilo', 'Daniel', 'Erico']
+console.log(chamada)
+
+let grupo1 = chamada.slice (0, chamada.legth/2);
+let grupo2 = chamada.slice(chamada.length/2)
+
+console.log(grupo1)
+console.log(grupo2)
---
+++ arraySubstituir.js
@@ -0,0 +1,4 @@
+let chamada = ['leo', 'davi', 'vk', 'lai']
+chamada.splice(1,2, 'nicolly')
+
+console.log(chamada)
